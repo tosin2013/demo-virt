5ce60263e981320e962a7835da24fd83bef335bd
Tosin Akinosho <takinosh@redhat.com>
2024-04-27 18:07:48 -0400

    testing router script

--- demo.redhat.com/docs/step1.md
+++ demo.redhat.com/docs/step1.md
@@ -1,11 +1,59 @@
 # Configure Networking on the Host
 
 
+
 ![20240427110356](https://i.imgur.com/PdnTynQ.png)
 ![20240427100635](https://i.imgur.com/ZJzE1Lp.png)
 ![20240427100654](https://i.imgur.com/aAmRFWl.png)
+### Validate the vyos
+**Default username and password**
+* username: vyos
+* password: vyos
 ![20240427101101](https://i.imgur.com/FA2rwXB.png)
+*install image*
 ![20240427101254](https://i.imgur.com/7yLOY8J.png)
 ![20240427101353](https://i.imgur.com/vmXQ8TE.png)
 ![20240427101428](https://i.imgur.com/PHT8DFo.png)
 ![20240427101509](https://i.imgur.com/Tp970x3.png)
+
+Configure External interface
+*if you are using the default kvm network the ip address can be 192..168.122.x*
+```bash
+$ configure
+$ set interfaces ethernet eth0 address 192.168.122.2/24
+$ set interfaces ethernet eth0 description Internet-Facing
+$ commit
+$ save
+$ run show interfaces
+$ set protocols static route 0.0.0.0/0 next-hop 192.168.122.1
+$ commit 
+$ run ping 1.1.1.1 interface 192.168.122.2
+$ save 
+$ exit 
+```
+
+Enable SSH on router
+```bash
+$ configure 
+$ set service ssh
+$ commit 
+$ save
+$ exit
+``` 
+
+ssh into router
+```bash
+$ ssh vyos@192.168.122.2
+```
+
+
+copy script to router
+```bash
+$ vi vyos-config.sh
+$ chmod +x vyos-config.sh
+```
+
+run script
+```bash
+bash vyos-config.sh
+```
---
+++ demo.redhat.com/vyos-config-1.5.sh
@@ -0,0 +1,94 @@
+#!/bin/vbash
+#https://docs.vyos.io/en/latest/automation/command-scripting.html
+# https://sivasankar.org/2018/2066/vyos-virtual-router-for-home-lab-or-smb/?utm_source=pocket_mylist
+source /opt/vyatta/etc/functions/script-template
+run show interfaces
+set interfaces ethernet eth1 address 192.168.49.1/24
+set interfaces ethernet eth1 description ETH1
+set interfaces ethernet eth1 vif 1924 description 'Lab'
+set interfaces ethernet eth1 vif 1924 address '192.168.50.1/24'
+run show interfaces
+set interfaces ethernet eth2 address 192.168.51.1/24
+set interfaces ethernet eth2 description ETH2
+set interfaces ethernet eth2 vif 1925 description 'Disco'
+set interfaces ethernet eth2 vif 1925 address '192.168.52.1/24'
+run show interfaces
+set interfaces ethernet eth3 address 192.168.53.1/24
+set interfaces ethernet eth3 description ETH3
+set interfaces ethernet eth3 vif 1926 description 'Trans Proxy'
+set interfaces ethernet eth3 vif 1926 address '192.168.54.1/24'
+run show interfaces
+set interfaces ethernet eth4 address 192.168.55.1/24
+set interfaces ethernet eth4 description ETH4
+set interfaces ethernet eth4 vif 1927 description 'Metal'
+set interfaces ethernet eth4 vif 1927 address '192.168.56.1/24'
+run show interfaces
+set interfaces ethernet eth5 address 192.168.57.1/24
+set interfaces ethernet eth5 description ETH5
+set interfaces ethernet eth5 vif 1928 description 'Provisioning'
+set interfaces ethernet eth5 vif 1928 address '192.168.58.1/24'
+run show interfaces
+set interfaces ethernet eth11 address 10.46.27.254/23
+set interfaces ethernet eth11 description FLOATING_IPS
+run show interfaces
+set nat source rule 10 outbound-interface 'eth0'
+set nat source rule 10 source address 10.46.27.254/23
+set nat source rule 10 translation address masquerade
+show nat source
+commit
+run ping  1.1.1.1 count 3 interface 10.46.27.254
+set nat source rule 11 outbound-interface 'eth0'
+set nat source rule 11 source address 192.168.50.0/24
+set nat source rule 11 translation address masquerade
+show nat source
+commit
+run ping  1.1.1.1 count 3 interface 192.168.50.1
+set nat source rule 12 outbound-interface 'eth0'
+set nat source rule 12 source address 192.168.52.0/24
+set nat source rule 12 translation address masquerade
+show nat source
+commit
+run ping  1.1.1.1 count 3 interface 192.168.52.1
+set nat source rule 13 outbound-interface 'eth0'
+set nat source rule 13 source address 192.168.54.0/24
+set nat source rule 13 translation address masquerade
+show nat source
+commit
+run ping  1.1.1.1 count 3 interface 192.168.54.1
+set service dhcp-server shared-network-name Lab subnet 192.168.50.0/24 default-router '192.168.50.1'
+set service dhcp-server shared-network-name Lab subnet 192.168.50.0/24 name-server '10.1.240.46'
+set service dhcp-server shared-network-name Lab subnet 192.168.50.0/24 domain-name 'lab.tosins-testing.io'
+set service dhcp-server shared-network-name Lab subnet 192.168.50.0/24 lease '86400'
+set service dhcp-server shared-network-name Lab subnet 192.168.50.0/24 range 0 start 192.168.50.100
+set service dhcp-server shared-network-name Lab subnet 192.168.50.0/24 range 0 stop '192.168.50.199'
+commit 
+set service dhcp-server shared-network-name Disco subnet 192.168.52.0/24 default-router '192.168.52.1'
+set service dhcp-server shared-network-name Disco subnet 192.168.52.0/24 name-server '10.1.240.46'
+set service dhcp-server shared-network-name Disco subnet 192.168.52.0/24 domain-name 'lab.tosins-testing.io'
+set service dhcp-server shared-network-name Disco subnet 192.168.52.0/24 lease '86400'
+set service dhcp-server shared-network-name Disco subnet 192.168.52.0/24 range 0 start 192.168.52.100
+set service dhcp-server shared-network-name Disco subnet 192.168.52.0/24 range 0 stop '192.168.52.199'
+commit 
+set service dhcp-server shared-network-name Trans Proxy subnet 192.168.54.0/24 default-router '192.168.54.1'
+set service dhcp-server shared-network-name Trans Proxy subnet 192.168.54.0/24 name-server '10.1.240.46'
+set service dhcp-server shared-network-name Trans Proxy subnet 192.168.54.0/24 domain-name 'lab.tosins-testing.io'
+set service dhcp-server shared-network-name Trans Proxy subnet 192.168.54.0/24 lease '86400'
+set service dhcp-server shared-network-name Trans Proxy subnet 192.168.54.0/24 range 0 start 192.168.54.100
+set service dhcp-server shared-network-name Trans Proxy subnet 192.168.54.0/24 range 0 stop '192.168.54.199'
+commit 
+set service dhcp-server shared-network-name Metal subnet 192.168.56.0/24 default-router '192.168.56.1'
+set service dhcp-server shared-network-name Metal subnet 192.168.56.0/24 name-server '10.1.240.46'
+set service dhcp-server shared-network-name Metal subnet 192.168.56.0/24 domain-name 'lab.tosins-testing.io'
+set service dhcp-server shared-network-name Metal subnet 192.168.56.0/24 lease '86400'
+set service dhcp-server shared-network-name Metal subnet 192.168.56.0/24 range 0 start 192.168.56.100
+set service dhcp-server shared-network-name Metal subnet 192.168.56.0/24 range 0 stop '192.168.56.199'
+commit 
+set service dhcp-server shared-network-name Provisioning subnet 192.168.58.0/24 default-router '192.168.58.1'
+set service dhcp-server shared-network-name Provisioning subnet 192.168.58.0/24 name-server '10.1.240.46'
+set service dhcp-server shared-network-name Provisioning subnet 192.168.58.0/24 domain-name 'lab.tosins-testing.io'
+set service dhcp-server shared-network-name Provisioning subnet 192.168.58.0/24 lease '86400'
+set service dhcp-server shared-network-name Provisioning subnet 192.168.58.0/24 range 0 start 192.168.58.100
+set service dhcp-server shared-network-name Provisioning subnet 192.168.58.0/24 range 0 stop '192.168.58.199'
+commit 
+save
+exit
