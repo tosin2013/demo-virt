4f37006e02423da150ada5d1ac183a0859b0d691
Tosin Akinosho <takinosh@redhat.com>
2024-04-28 20:27:36 -0400

    updating code

--- demo.redhat.com/docs/step1.md
+++ demo.redhat.com/docs/step1.md
@@ -1,22 +1,31 @@
 # Configure Networking on the Host
 
+## Deploy DNS Server on Equinix Metal
+[Deploy freeipa vm](https://tosin2013.github.io/qubinode_navigator/plugins/onedev-kcli-pipelines.html)
 
-
+## Deploy vyos router on KVM
+*When vm has deployed you will see the folowing*
 ![20240427110356](https://i.imgur.com/PdnTynQ.png)
-![20240427100635](https://i.imgur.com/ZJzE1Lp.png)
+---
+![20240427203404](https://i.imgur.com/IIq2kZI.png)
+---
 ![20240427100654](https://i.imgur.com/aAmRFWl.png)
 ### Validate the vyos
 **Default username and password**
 * username: vyos
 * password: vyos
 ![20240427101101](https://i.imgur.com/FA2rwXB.png)
-*install image*
+### Run command : install image
 ![20240427101254](https://i.imgur.com/7yLOY8J.png)
+---
 ![20240427101353](https://i.imgur.com/vmXQ8TE.png)
+---
 ![20240427101428](https://i.imgur.com/PHT8DFo.png)
+---
 ![20240427101509](https://i.imgur.com/Tp970x3.png)
+**The vm will shut down and you must manually start it**
 
-Configure External interface
+## Configure External interface
 *if you are using the default kvm network the ip address can be 192..168.122.x*
 ```bash
 $ configure
@@ -32,7 +41,7 @@ $ save
 $ exit 
 ```
 
-Enable SSH on router
+**Enable SSH on router**
 ```bash
 $ configure 
 $ set service ssh
@@ -47,14 +56,73 @@ $ ssh vyos@192.168.122.2
 ```
 
 
-copy script to router
+**Copy script to router**
 * link to script [vyos-config.sh](https://github.com/tosin2013/demo-virt/blob/rhpds/demo.redhat.com/vyos-config-1.5.sh)
+* If are using not 192.168.122.2 as the ip address of the route or would like to edit items like dns server, you can edit the script
+* Update the script to use the ip address from the created DNS Server in vim `:%s/1.1.1.1/newip/g`
 ```bash
 $ vi vyos-config.sh
 $ chmod +x vyos-config.sh
 ```
 
-run script
+**run script**
 ```bash
-bash vyos-config.sh
+vbash vyos-config.sh
+```
+
+**Add routes to linux server**
+```
+sudo ip route add 192.168.49.0/24 via 192.168.122.2
+sudo ip route add 192.168.50.0/24 via 192.168.122.2
+sudo ip route add 192.168.51.0/24 via 192.168.122.2
+sudo ip route add 192.168.52.0/24 via 192.168.122.2
+sudo ip route add 192.168.53.0/24 via 192.168.122.2
+sudo ip route add 192.168.54.0/24 via 192.168.122.2
+sudo ip route add 192.168.55.0/24 via 192.168.122.2
+sudo ip route add 192.168.56.0/24 via 192.168.122.2
+sudo ip route add 192.168.57.0/24 via 192.168.122.2
+sudo ip route add 192.168.58.0/24 via 192.168.122.2
+```
+
+**nmcli script to add routes permanently**
+```bash
+#!/bin/bash
+
+# Display available NetworkManager connections
+echo "Available NetworkManager connections:"
+nmcli con show
+
+# Prompt user to enter the profile name
+read -p "Enter the name of the NetworkManager profile you wish to modify: " profile_name
+
+# Confirm the profile name
+nmcli con show "$profile_name" &>/dev/null
+if [ $? -ne 0 ]; then
+    echo "Profile '$profile_name' not found. Please check the profile name and try again."
+    exit 1
+fi
+
+# Define the routes to be added
+declare -a routes=(
+    "192.168.49.0/24 192.168.122.2"
+    "192.168.50.0/24 192.168.122.2"
+    "192.168.51.0/24 192.168.122.2"
+    "192.168.52.0/24 192.168.122.2"
+    "192.168.53.0/24 192.168.122.2"
+    "192.168.54.0/24 192.168.122.2"
+    "192.168.55.0/24 192.168.122.2"
+    "192.168.56.0/24 192.168.122.2"
+    "192.168.57.0/24 192.168.122.2"
+    "192.168.58.0/24 192.168.122.2"
+)
+
+# Add routes to the profile
+for route in "${routes[@]}"; do
+    nmcli connection modify "$profile_name" +ipv4.routes "$route"
+done
+
+# Restart the NetworkManager to apply changes
+echo "Applying changes..."
+sudo systemctl restart NetworkManager
+echo "Routes added successfully to the profile '$profile_name'."
 ```
--- demo.redhat.com/vyos-config-1.5.sh
+++ demo.redhat.com/vyos-config-1.5.sh
@@ -2,6 +2,7 @@
 #https://docs.vyos.io/en/latest/automation/command-scripting.html
 # https://sivasankar.org/2018/2066/vyos-virtual-router-for-home-lab-or-smb/?utm_source=pocket_mylist
 source /opt/vyatta/etc/functions/script-template
+configure
 run show interfaces
 set interfaces ethernet eth1 address 192.168.49.1/24
 set interfaces ethernet eth1 description ETH1
@@ -15,7 +16,7 @@ set interfaces ethernet eth2 vif 1925 address '192.168.52.1/24'
 run show interfaces
 set interfaces ethernet eth3 address 192.168.53.1/24
 set interfaces ethernet eth3 description ETH3
-set interfaces ethernet eth3 vif 1926 description 'Trans Proxy'
+set interfaces ethernet eth3 vif 1926 description 'Trans-Proxy'
 set interfaces ethernet eth3 vif 1926 address '192.168.54.1/24'
 run show interfaces
 set interfaces ethernet eth4 address 192.168.55.1/24
@@ -28,67 +29,75 @@ set interfaces ethernet eth5 description ETH5
 set interfaces ethernet eth5 vif 1928 description 'Provisioning'
 set interfaces ethernet eth5 vif 1928 address '192.168.58.1/24'
 run show interfaces
-set interfaces ethernet eth11 address 10.46.27.254/23
-set interfaces ethernet eth11 description FLOATING_IPS
-run show interfaces
-set nat source rule 10 outbound-interface 'eth0'
-set nat source rule 10 source address 10.46.27.254/23
+set nat source rule 10 outbound-interface name 'eth0'
+set nat source rule 10 source address 192.168.122.2
 set nat source rule 10 translation address masquerade
 show nat source
 commit
-run ping  1.1.1.1 count 3 interface 10.46.27.254
-set nat source rule 11 outbound-interface 'eth0'
+run ping  1.1.1.1 count 3 interface 192.168.122.2
+set nat source rule 11 outbound-interface name 'eth0'
+set nat source rule 11 source address 192.168.49.0/24
+set nat source rule 11 translation address masquerade
+show nat source
+commit
+run ping  1.1.1.1 count 3 interface 192.168.49.1
+set nat source rule 11 outbound-interface name 'eth0'
 set nat source rule 11 source address 192.168.50.0/24
 set nat source rule 11 translation address masquerade
 show nat source
 commit
 run ping  1.1.1.1 count 3 interface 192.168.50.1
-set nat source rule 12 outbound-interface 'eth0'
+set nat source rule 12 outbound-interface name 'eth0'
 set nat source rule 12 source address 192.168.52.0/24
 set nat source rule 12 translation address masquerade
 show nat source
 commit
 run ping  1.1.1.1 count 3 interface 192.168.52.1
-set nat source rule 13 outbound-interface 'eth0'
+set nat source rule 13 outbound-interface name 'eth0'
 set nat source rule 13 source address 192.168.54.0/24
 set nat source rule 13 translation address masquerade
 show nat source
 commit
 run ping  1.1.1.1 count 3 interface 192.168.54.1
-set service dhcp-server shared-network-name Lab subnet 192.168.50.0/24 default-router '192.168.50.1'
-set service dhcp-server shared-network-name Lab subnet 192.168.50.0/24 name-server '1.1.1.1'
-set service dhcp-server shared-network-name Lab subnet 192.168.50.0/24 domain-name 'lab.example.com'
+set service dhcp-server shared-network-name Lab subnet 192.168.50.0/24 option default-router '192.168.50.1'
+set service dhcp-server shared-network-name Lab subnet 192.168.50.0/24 option name-server '1.1.1.1'
+set service dhcp-server shared-network-name Lab subnet 192.168.50.0/24 option domain-name 'example.com'
 set service dhcp-server shared-network-name Lab subnet 192.168.50.0/24 lease '86400'
 set service dhcp-server shared-network-name Lab subnet 192.168.50.0/24 range 0 start 192.168.50.100
 set service dhcp-server shared-network-name Lab subnet 192.168.50.0/24 range 0 stop '192.168.50.199'
+set service dhcp-server shared-network-name Lab subnet 192.168.50.0/24 subnet-id '1'
 commit 
-set service dhcp-server shared-network-name Disco subnet 192.168.52.0/24 default-router '192.168.52.1'
-set service dhcp-server shared-network-name Disco subnet 192.168.52.0/24 name-server '1.1.1.1'
-set service dhcp-server shared-network-name Disco subnet 192.168.52.0/24 domain-name 'lab.example.com'
+set service dhcp-server shared-network-name Disco subnet 192.168.52.0/24 option default-router '192.168.52.1'
+set service dhcp-server shared-network-name Disco subnet 192.168.52.0/24 option name-server '1.1.1.1'
+set service dhcp-server shared-network-name Disco subnet 192.168.52.0/24 option domain-name 'example.com'
 set service dhcp-server shared-network-name Disco subnet 192.168.52.0/24 lease '86400'
 set service dhcp-server shared-network-name Disco subnet 192.168.52.0/24 range 0 start 192.168.52.100
 set service dhcp-server shared-network-name Disco subnet 192.168.52.0/24 range 0 stop '192.168.52.199'
+set service dhcp-server shared-network-name Disco subnet 192.168.52.0/24 subnet-id '2'
 commit 
-set service dhcp-server shared-network-name Trans Proxy subnet 192.168.54.0/24 default-router '192.168.54.1'
-set service dhcp-server shared-network-name Trans Proxy subnet 192.168.54.0/24 name-server '1.1.1.1'
-set service dhcp-server shared-network-name Trans Proxy subnet 192.168.54.0/24 domain-name 'lab.example.com'
-set service dhcp-server shared-network-name Trans Proxy subnet 192.168.54.0/24 lease '86400'
-set service dhcp-server shared-network-name Trans Proxy subnet 192.168.54.0/24 range 0 start 192.168.54.100
-set service dhcp-server shared-network-name Trans Proxy subnet 192.168.54.0/24 range 0 stop '192.168.54.199'
+set service dhcp-server shared-network-name Trans-Proxy subnet 192.168.54.0/24 option default-router '192.168.54.1'
+set service dhcp-server shared-network-name Trans-Proxy subnet 192.168.54.0/24 option name-server '1.1.1.1'
+set service dhcp-server shared-network-name Trans-Proxy subnet 192.168.54.0/24 option domain-name 'example.com'
+set service dhcp-server shared-network-name Trans-Proxy subnet 192.168.54.0/24 lease '86400'
+set service dhcp-server shared-network-name Trans-Proxy subnet 192.168.54.0/24 range 0 start 192.168.54.100
+set service dhcp-server shared-network-name Trans-Proxy subnet 192.168.54.0/24 range 0 stop '192.168.54.199'
+set service dhcp-server shared-network-name Trans-Proxy subnet 192.168.54.0/24 subnet-id '3'
 commit 
-set service dhcp-server shared-network-name Metal subnet 192.168.56.0/24 default-router '192.168.56.1'
-set service dhcp-server shared-network-name Metal subnet 192.168.56.0/24 name-server '1.1.1.1'
-set service dhcp-server shared-network-name Metal subnet 192.168.56.0/24 domain-name 'lab.example.com'
+set service dhcp-server shared-network-name Metal subnet 192.168.56.0/24 option default-router '192.168.56.1'
+set service dhcp-server shared-network-name Metal subnet 192.168.56.0/24 option name-server '1.1.1.1'
+set service dhcp-server shared-network-name Metal subnet 192.168.56.0/24 option domain-name 'example.com'
 set service dhcp-server shared-network-name Metal subnet 192.168.56.0/24 lease '86400'
 set service dhcp-server shared-network-name Metal subnet 192.168.56.0/24 range 0 start 192.168.56.100
 set service dhcp-server shared-network-name Metal subnet 192.168.56.0/24 range 0 stop '192.168.56.199'
+set service dhcp-server shared-network-name Metal subnet 192.168.56.0/24 subnet-id '4'
 commit 
-set service dhcp-server shared-network-name Provisioning subnet 192.168.58.0/24 default-router '192.168.58.1'
-set service dhcp-server shared-network-name Provisioning subnet 192.168.58.0/24 name-server '1.1.1.1'
-set service dhcp-server shared-network-name Provisioning subnet 192.168.58.0/24 domain-name 'lab.example.com'
+set service dhcp-server shared-network-name Provisioning subnet 192.168.58.0/24 option default-router '192.168.58.1'
+set service dhcp-server shared-network-name Provisioning subnet 192.168.58.0/24 option name-server '1.1.1.1'
+set service dhcp-server shared-network-name Provisioning subnet 192.168.58.0/24 option domain-name 'example.com'
 set service dhcp-server shared-network-name Provisioning subnet 192.168.58.0/24 lease '86400'
 set service dhcp-server shared-network-name Provisioning subnet 192.168.58.0/24 range 0 start 192.168.58.100
 set service dhcp-server shared-network-name Provisioning subnet 192.168.58.0/24 range 0 stop '192.168.58.199'
-commit 
+set service dhcp-server shared-network-name Provisioning subnet 192.168.58.0/24 subnet-id '5'
+commit
 save
 exit
